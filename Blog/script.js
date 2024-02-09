398087621fa3e71cf4de8f66409d9a84f82f7237
SakhiMeyiwa <159584750+SakhiMeyiwa@users.noreply.github.com>
2024-02-09 16:47:14 -0700

    Add files via upload
    files

---
+++ Blog/script.js
@@ -0,0 +1,14 @@
+
+document.querySelector('.Open').addEventListener('click',()=>{
+    document.querySelector('.nav-link').style.display='flex';
+    document.querySelector('.Open').style.display='none'
+    document.querySelector('.Close').style.display='inline-block'
+})
+
+
+
+document.querySelector('.Close').addEventListener('click',()=>{
+    document.querySelector('.nav-link').style.display='none';
+    document.querySelector('.Open').style.display='inline-block'
+    document.querySelector('.Close').style.display='none'
+})
